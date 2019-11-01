93c8d37bc6744f7a8b16ecd29b9f792dd5d75c63
Dustin Thostenson <sourcetree@delta3consulting.com>
2019-10-31 20:30:36 -0400

    TBD kata at break.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,6 +4,10 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
+            when (items[i].name) {
+                    "Backstage passes to a TAFKAL80ETC concert" ->updateBackStageQuality()
+            }
+
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -15,16 +19,14 @@ class GildedRose(var items: Array<Item>) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
-                            }
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                      // combining mutliple nested if statement or conditions.
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                           // if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
-                            }
+                           // }
                         }
                     }
                 }
@@ -42,16 +44,21 @@ class GildedRose(var items: Array<Item>) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
-                    } else {
+                        //Make items[i].quality = 0
+                    } else { // BSP quality is 0
                         items[i].quality = items[i].quality - items[i].quality
                     }
-                } else {
+                } else { // Brie should be incremented by 1
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
             }
         }
+    }
+
+    private fun updateBackStageQuality() {
+
     }
 
 }
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,70 @@
 package com.gildedrose
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    // the item is a backstage ticket and there's more than 10 days until
+    @Test
+    fun isBackStage_withQualitySignificantlyLessThanFiftyAndSellMoreThanTen_incrementQualityByOne() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 15, 30))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        assertEquals(31, app.items[0].quality)
+    }
+
+    //quality is already at max which is 50.
+    @Test
+    fun withQualityEqualsFifty_incrementQualityByZero() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun isBackStage_withQualityLessThanFortyNineAndSellInLessThanEleven_incrementQualityByTwo() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 48))
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun isBackStage_withQualityLessThanFortyEightAndSellInLessThanSix_incrementQualityByThree() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 3, 42))
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        assertEquals(45, app.items[0].quality)
     }
 
 
+    @Test
+    fun isBackStage_withListOfItems_incrementQualityByThree() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 3, 49),
+                Item("Backstage passes to a TAFKAL80ETC concert", 3, 48),
+                Item("Backstage passes to a TAFKAL80ETC concert", 3, 46))
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        assertEquals(50, app.items[0].quality)
+        assertEquals(50, app.items[1].quality)
+        assertEquals(49, app.items[2].quality)
+    }
+
+    @Test
+    fun isBackStage_withSellInZero_makeQualityAsZero() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 42))
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        assertEquals(0, app.items[0].quality)
+    }
 }
 
 
