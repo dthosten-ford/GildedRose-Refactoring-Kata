a281956883ff5bfd8d4397c8c22b77b66863daa2
John Smith <jsmi1417@ford.com>
2021-01-12 10:39:34 -0600

    finish assert changes after update refactor

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -32,41 +32,27 @@ class GildedRoseTests: XCTestCase {
     }
 
     func testMaxQuality() {
-        let items = [Item(name: "Aged Brie", sellIn: 1, quality: 50)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items.first?.quality, 50)
+        let item = Item(name: "Aged Brie", sellIn: 1, quality: 50)
+        assertChangesAfterUpdate(item: item, expectedQuality: 50, expectedSellIn: 0)
     }
 
     func testSulfurasDoesNotChangeQualityOrSellIn() {
-        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 30)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items.first?.quality, 30)
-        XCTAssertEqual(items.first?.sellIn, 0)
+        let item = Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 30)
+        assertChangesAfterUpdate(item: item, expectedQuality: 30, expectedSellIn: 0)
     }
     
     func testBackstageIncreasingQualityByTwoWhenLessThanTenDays() {
-        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items.first?.quality, 2)
-        XCTAssertEqual(items.first?.sellIn, 9)
+        let item = Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 0)
+        assertChangesAfterUpdate(item: item, expectedQuality: 2, expectedSellIn: 9)
     }
     
     func testBackstageIncreasingQualityByThreeWhenLessThanFiveDays() {
-        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items.first?.quality, 3)
-        XCTAssertEqual(items.first?.sellIn, 4)
+        let item = Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 0)
+        assertChangesAfterUpdate(item: item, expectedQuality: 3, expectedSellIn: 4)
     }
     
     func testBackstageNoQualityAfterEventFinished() {
-        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 90)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items.first?.quality, 0)
-        XCTAssertEqual(items.first?.sellIn, -1)
+        let item = Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 90)
+        assertChangesAfterUpdate(item: item, expectedQuality: 0, expectedSellIn: -1)
     }
 }
