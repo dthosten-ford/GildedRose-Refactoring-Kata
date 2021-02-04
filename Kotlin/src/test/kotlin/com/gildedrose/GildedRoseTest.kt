d359c3e46a54de565aed958d057c25ef343427e8
DustinThostenson <dustin@delta3consulting.com>
2021-02-04 14:55:29 -0600

    Revert "Merge branch 'boomer' of https://github.com/dthosten-ford/GildedRose-Refactoring-Kata into boomer"
    This reverts commit 714b0ce36e4fac30c5b7e1a782fb78e76f2236f2, reversing
    changes made to 81eff73e39857b37750ba3dbf027e07c1a857f19.

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,57 @@
 package com.gildedrose
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
-    fun testItemQualityIsNeverNegative() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertTrue(items[0].quality >= 0)
+    private fun getGildedRoseWithItems(name: String, sellIn: Int, quality: Int): GildedRose {
+        val items = arrayOf(Item(name, sellIn, quality))
+        return GildedRose(items)
     }
 
+    @Test
+    fun testItemQualityIsNeverNegative() {
+        val app = getGildedRoseWithItems("foo", 0, 0)
+        app.updateQuality()
+        assertTrue(app.items[0].quality >= 0)
+    }
+
+    @Test
+    fun testItemQualityIsNeverMoreThan50() {
+        val app = getGildedRoseWithItems("Aged Brie", 0, 50)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testAgedBrieShouldIncreaseQualityBy2() {
+        val app = getGildedRoseWithItems("Aged Brie", 0, 48)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testSystemLowersBothValuesForEveryItemAtTheEndOfEachDay() {
+        val app = getGildedRoseWithItems("ASDF", 0, 49)
+        app.updateQuality()
+        assertEquals(47, app.items[0].quality)
+        assertEquals(-1, app.items[0].sellIn)
+    }
+
+    @Test
+    fun testItemQualityIncreasesTo50WhileSellInLessThan6() {
+        val app =  getGildedRoseWithItems("Backstage passes to a TAFKAL80ETC concert", 5, 45)
+        app.updateQuality()
+        assertEquals(48, app.items[0].quality)
+    }
+
+    @Test
+    fun testItemQualityBecomesZeroAfterConcertIfItIsBackstage(){
+        val app = getGildedRoseWithItems("Backstage passes to a TAFKAL80ETC concert", 0, 45)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
 }
 
 
