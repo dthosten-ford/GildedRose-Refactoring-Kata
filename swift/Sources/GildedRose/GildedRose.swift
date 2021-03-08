06f61348896ad400b0864bf4c3e5612b65a9d730
Mohit Gupta <mgupta27@ford.com>
2021-03-08 10:02:44 -0500

    refactor: max quality logic into a method

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -7,13 +7,19 @@ public class GildedRose {
         self.items = items
     }
     
+    fileprivate func incrementQltyIfLessThanMaxQlty(_ item: Item) {
+        if (item.quality < maxQuality) {
+            item.quality = item.quality + 1
+        }
+    }
+    
     public func updateQuality() {
-        for i in 0..<items.count {
+        for item in items {
             /*
              1) Too much nested if statement
              2) hard to understand and scattered logic
              3) needs methods to do the operations
-             4) items[i] can be moved to a constant
+             4) item can be moved to a constant
              5) magic numbers with no explanation
              6) repeatitive conditions
              7) literals in the code
@@ -21,51 +27,44 @@ public class GildedRose {
              9)
              */
             
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > minQuality) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > minQuality) {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < maxQuality) {
-                    items[i].quality = items[i].quality + 1
+
+                incrementQltyIfLessThanMaxQlty(item)
+                
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn < 11) {
+                        incrementQltyIfLessThanMaxQlty(item)
+                    }
                     
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < maxQuality) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < maxQuality) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+                    if (item.sellIn < 6) {
+                        incrementQltyIfLessThanMaxQlty(item)
                     }
                 }
             }
             
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
             
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > minQuality) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > minQuality) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < maxQuality) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    incrementQltyIfLessThanMaxQlty(item)
                 }
             }
         }
