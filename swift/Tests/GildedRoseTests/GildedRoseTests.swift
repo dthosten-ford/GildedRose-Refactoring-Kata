992e0aa34a97fe79460fe6b41c645cffc54477bd
Mohit Gupta <mgupta27@ford.com>
2021-04-06 09:56:22 -0400

    refactor: added tests for sulfuras

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -4,6 +4,7 @@ import XCTest
 class GildedRoseTests: XCTestCase {
     
     var regularItem: Item { Item(name: "regular", sellIn: 25, quality: 25) }
+    var sulfurasItem: Item { Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 25, quality: 25) }
 
     func test_regularItem_qualityShouldDecreaseByOne() {
         let items = [regularItem]
@@ -19,4 +20,17 @@ class GildedRoseTests: XCTestCase {
         XCTAssertEqual(items[0].sellIn, regularItem.sellIn - 1)
     }
     
+    func testSulfurasItem() {
+        let items = [sulfurasItem]
+        let app = GildedRose(items: items)
+        app.updateQuality();
+        XCTAssertEqual(items[0].sellIn, sulfurasItem.sellIn)
+    }
+    
+    func testSulfrusItemQualityShouldDecrement() {
+        let items = [sulfurasItem]
+        let app = GildedRose(items: items)
+        app.updateQuality();
+        XCTAssertEqual(items[0].quality, sulfurasItem.quality)
+    }
 }
