32631ebb5207ab4fbd6b9fed916e9a3935731a1e
Mohit Gupta <mgupta27@ford.com>
2021-04-06 10:12:36 -0400

    refactor: refactor tests

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,35 +2,38 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    
     var regularItem: Item { Item(name: "regular", sellIn: 25, quality: 25) }
     var sulfurasItem: Item { Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 25, quality: 25) }
 
-    func test_regularItem_qualityShouldDecreaseByOne() {
-        let items = [regularItem]
+    fileprivate func update(item: Item) -> Item {
+        let items = [item]
         let app = GildedRose(items: items)
-        app.updateQuality();
-        XCTAssertEqual(items[0].quality, regularItem.quality - 1)
+        app.updateQuality()
+        return items[0]
     }
     
-    func test_regualItem_sellInShouldBeSomething() {
+    func test_regularItem_qualityShouldDecreaseBy1() {
+        XCTAssertEqual(update(item: regularItem).quality, regularItem.quality - 1)
+    }
+    
+    func test_regularItem_sellInShouldDecreaseBy1() {
         let items = [regularItem]
         let app = GildedRose(items: items)
-        app.updateQuality();
+        app.updateQuality()
         XCTAssertEqual(items[0].sellIn, regularItem.sellIn - 1)
     }
     
-    func testSulfurasItem() {
+    func test_sulfurasItem_sellInShouldBeUnchanged() {
         let items = [sulfurasItem]
         let app = GildedRose(items: items)
-        app.updateQuality();
+        app.updateQuality()
         XCTAssertEqual(items[0].sellIn, sulfurasItem.sellIn)
     }
     
-    func testSulfrusItemQualityShouldDecrement() {
+    func test_sulfurasItem_qualityShouldBeUnchanged() {
         let items = [sulfurasItem]
         let app = GildedRose(items: items)
-        app.updateQuality();
+        app.updateQuality()
         XCTAssertEqual(items[0].quality, sulfurasItem.quality)
     }
 }
