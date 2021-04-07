02929808ca336d2253aa482f99c27769059ad9cc
Chris Thiebaut <cthieba3@ford.com>
2021-04-07 09:35:45 -0400

    Reduce duplication and add AgedBrie quality test

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -4,36 +4,36 @@ import XCTest
 class GildedRoseTests: XCTestCase {
     var regularItem: Item { Item(name: "regular", sellIn: 25, quality: 25) }
     var sulfurasItem: Item { Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 25, quality: 25) }
-
-    fileprivate func update(item: Item) -> Item {
-        let items = [item]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        return items[0]
-    }
-    
+    var agedBrie: Item { Item(name: "Aged Brie", sellIn: 25, quality: 25) }
+ 
     func test_regularItem_qualityShouldDecreaseBy1() {
         XCTAssertEqual(update(item: regularItem).quality, regularItem.quality - 1)
     }
     
     func test_regularItem_sellInShouldDecreaseBy1() {
-        let items = [regularItem]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items[0].sellIn, regularItem.sellIn - 1)
+        let updatedItem = update(item: regularItem)
+        XCTAssertEqual(updatedItem.sellIn, regularItem.sellIn - 1)
     }
     
     func test_sulfurasItem_sellInShouldBeUnchanged() {
-        let items = [sulfurasItem]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(items[0].sellIn, sulfurasItem.sellIn)
+        let updatedItem = update(item: sulfurasItem)
+        XCTAssertEqual(updatedItem.sellIn, sulfurasItem.sellIn)
     }
     
     func test_sulfurasItem_qualityShouldBeUnchanged() {
-        let items = [sulfurasItem]
+        let updatedItem = update(item: sulfurasItem)
+        XCTAssertEqual(updatedItem.quality, sulfurasItem.quality)
+    }
+    
+    func test_agedBrie_qualityShouldIncreaseBy1() {
+        let updatedItem = update(item: agedBrie)
+        XCTAssertEqual(updatedItem.quality, agedBrie.quality + 1)
+    }
+    
+    fileprivate func update(item: Item) -> Item {
+        let items = [item]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(items[0].quality, sulfurasItem.quality)
+        return items[0]
     }
 }
