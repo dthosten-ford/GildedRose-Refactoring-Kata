674f424a525dbfc612db6885178d360ecae4e5e4
Amit Nalawade <analawad@ford.com>
2021-04-07 09:50:18 -0400

    test for backstage pass quality

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -5,6 +5,7 @@ class GildedRoseTests: XCTestCase {
     var regularItem: Item { Item(name: "regular", sellIn: 10, quality: 15) }
     var sulfurasItem: Item { Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 10) }
     var agedBrie: Item { Item(name: "Aged Brie", sellIn: 2, quality: 8) }
+    var backstagePass: Item { Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 25)}
  
     func test_regularItem_qualityShouldDecreaseBy1() {
         XCTAssertEqual(update(item: regularItem).quality, regularItem.quality - 1)
@@ -33,6 +34,11 @@ class GildedRoseTests: XCTestCase {
     func test_agedBrie_sellInShouldDecreaseBy1() {
         let updatedItem = update(item: agedBrie)
         XCTAssertEqual(updatedItem.sellIn, agedBrie.sellIn - 1)
+    }
+
+    func test_backstagePass_qualityShouldIncreaseBy1() {
+        let updatedItem = update(item: backstagePass)
+        XCTAssertEqual(updatedItem.quality, backstagePass.quality + 1)
     }
     
     fileprivate func update(item: Item) -> Item {
