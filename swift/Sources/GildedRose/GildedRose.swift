5568c632437b623c166e3e059fcbd5c6f7cff0ed
Moises <MCASTR74@ford.com>
2021-04-14 09:49:23 -0400

    katas: add list of TODO, and remove magic numbers

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,33 +1,41 @@
 public class GildedRose {
     var items:[Item]
+    private let maxItemQuality = 50
+    private let minItemQuality = 0
     
     public init(items:[Item]) {
         self.items = items
     }
     
-    //TODO:
+    // TODO:
+    // * Getting rid of magic numbers
+    // * Remove repetitive if conditions
+    // * Literal strings
+    // * Extract repetitive functionality into methods
+    // * Rename indexer form array of items
+    // *
     
     public func updateQuality() {
         for i in 0..<items.count {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
+                if (items[i].quality > minItemQuality) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                         items[i].quality = items[i].quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < maxItemQuality) {
                     items[i].quality = items[i].quality + 1
                     
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < maxItemQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
                         
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < maxItemQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -42,7 +50,7 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
+                        if (items[i].quality > minItemQuality) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                                 items[i].quality = items[i].quality - 1
                             }
@@ -51,7 +59,7 @@ public class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < maxItemQuality) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
