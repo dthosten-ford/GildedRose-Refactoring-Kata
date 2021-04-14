e0779e857beebe3f9179d4ccff1e503d690966c4
Amit Nalawade <analawad@ford.com>
2021-04-14 09:56:58 -0400

    invert logic for aged brie

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -17,6 +17,9 @@ public class GildedRose {
     
     public func updateQuality() {
         for i in 0..<items.count {
+            if (items[i].name == "Sulfuras, Hand of Ragnaros") {
+                continue
+            }
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > minItemQuality) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
@@ -48,7 +51,11 @@ public class GildedRose {
             }
             
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
+                if (items[i].name == "Aged Brie") {
+                    if (items[i].quality < maxItemQuality) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                } else {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > minItemQuality) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
@@ -57,10 +64,6 @@ public class GildedRose {
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < maxItemQuality) {
-                        items[i].quality = items[i].quality + 1
                     }
                 }
             }
