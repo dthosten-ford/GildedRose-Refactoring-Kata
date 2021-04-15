2fec2b50f38c43c6a922f2d19bb8a4324f19fc9d
Moises <MCASTR74@ford.com>
2021-04-15 09:45:05 -0400

    katas: remove literals

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -2,17 +2,20 @@ public class GildedRose {
     var items:[Item]
     private let maxItemQuality = 50
     private let minItemQuality = 0
+    private let sulfuras = "Sulfuras, Hand of Ragnaros"
+    private let agedBrie = "Aged Brie"
+    private let backstage = "Backstage passes to a TAFKAL80ETC concert"
     
     public init(items:[Item]) {
         self.items = items
     }
     
     // TODO:
-    // * Getting rid of magic numbers
+    // ✅ Getting rid of magic numbers
     // * Remove repetitive if conditions
-    // * Literal strings
-    // * Extract repetitive functionality into methods
-    // * Rename indexer form array of items
+    // ✅ Literal strings
+    // ✅ Extract repetitive functionality into methods
+    // ✅ Rename indexer form array of items
     // *
     
     private func incrementQualityBy1(_ item: Item) {
@@ -29,17 +32,17 @@ public class GildedRose {
     
     public func updateQuality() {
         for item in items {
-            if (item.name == "Sulfuras, Hand of Ragnaros") {
+            if (item.name == sulfuras) {
                 continue
             }
 
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.name != agedBrie && item.name != backstage) {
                 decrementQualityBy1(item)
             } else {
                 if (item.quality < maxItemQuality) {
                     item.quality = item.quality + 1
                     
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name == backstage) {
                         if (item.sellIn < 11) {
                             incrementQualityBy1(item)
                         }
@@ -54,9 +57,9 @@ public class GildedRose {
             item.sellIn = item.sellIn - 1
             
             if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
+                if (item.name == agedBrie) {
                     incrementQualityBy1(item)
-                } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                } else if (item.name == backstage) {
                     item.quality = item.quality - item.quality
                 } else {
                     decrementQualityBy1(item)
