f43a154b220e9ddd7d054fa0169f160be765f51d
Chris Thiebaut <cthieba3@ford.com>
2021-04-27 10:06:06 -0400

    refactor: Remove duplication

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -45,50 +45,20 @@ class StrategyProvider: ItemStrategyProvider {
         if item.name == backstage {
             return BackstageStrategy()
         }
-        return LegacyItemUpdater()
+        return RegularItemUpdater()
     }
 }
 
-class LegacyItemUpdater: ItemUpdater {
-    private let maxItemQuality = 50
-    private let minItemQuality = 0
-    private let backstage = "Backstage passes to a TAFKAL80ETC concert"
+class RegularItemUpdater: ItemUpdater {
+    private let mutator = IncrementDecrementHolder()
     
     func updateItem(_ item: Item) {
-        if (item.name == backstage) {
-            incrementQualityBy1(item)
-
-            if (item.sellIn < 11) {
-                incrementQualityBy1(item)
-            }
-
-            if (item.sellIn < 6) {
-                incrementQualityBy1(item)
-            }
-        } else {
-            decrementQualityBy1(item)
-        }
+        mutator.decrementQualityBy1(item)
         
         item.sellIn = item.sellIn - 1
         
         if isExpired(item) {
-            if (item.name == backstage) {
-                item.quality = item.quality - item.quality
-            } else {
-                decrementQualityBy1(item)
-            }
-        }
-    }
-    
-    private func incrementQualityBy1(_ item: Item) {
-        if (item.quality < maxItemQuality) {
-            item.quality = item.quality + 1
-        }
-    }
-    
-    private func decrementQualityBy1(_ item: Item) {
-        if (item.quality > minItemQuality) {
-            item.quality = item.quality - 1
+            mutator.decrementQualityBy1(item)
         }
     }
     
@@ -102,30 +72,17 @@ class DoNothingStrategy: ItemUpdater {
 }
 
 class AgedBrieStrategy: ItemUpdater {
-    private let maxItemQuality = 50
-    private let minItemQuality = 0
-    
-    private func incrementQualityBy1(_ item: Item) {
-        if (item.quality < maxItemQuality) {
-            item.quality = item.quality + 1
-        }
-    }
-    
-    private func decrementQualityBy1(_ item: Item) {
-        if (item.quality > minItemQuality) {
-            item.quality = item.quality - 1
-        }
-    }
+    private let mutator = IncrementDecrementHolder()
     
     private func isExpired(_ item: Item) -> Bool {
         return item.sellIn < 0
     }
     
     func updateItem(_ item: Item) {
-        incrementQualityBy1(item)
+        mutator.incrementQualityBy1(item)
         item.sellIn = item.sellIn - 1
         if isExpired(item) {
-            incrementQualityBy1(item)
+            mutator.incrementQualityBy1(item)
         }
     }
 }
@@ -159,7 +116,6 @@ class BackstageStrategy: ItemUpdater {
 }
 
 class IncrementDecrementHolder {
-
     private let maxItemQuality = 50
     private let minItemQuality = 0
 
